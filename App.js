9c832532279b4bc94861b58ad75206c4468dafbc
GabrielBasitt <94151432+GabrielBasitt@users.noreply.github.com>
2022-06-07 14:17:07 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,38 +1,22 @@
-import React from 'react';
-import { View, Image, Text, Button, TouchableOpacity, Alert } from 'react-native';
-import styles from './src/styles/app'
+import React from 'react'
 
-const App = () => { 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/stack'
 
+import PaginaInicial from './src/componentes/PaginaInicial'
+
+
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  
   return(
-    <View style={styles.container}>
-      <Image
-        style={styles.tinyLogo}
-        source={require('./assets/LOGO.png')}
-      />
-    <View>
-      <Text style={styles.titulo}>
-      PaH
-      </Text>
-      <Text style={styles.frase}>
-        Pessoas são a esperança
-      </Text>
-      <Text style={styles.frase} >Seja Bem-vindo(a)</Text>
-    </View>
-    
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "PaginaInicial" component = {PaginaInicial} />
+      </Stack.Navigator>
 
-    <View >
-    <TouchableOpacity
-    style={styles.btnLC}
-    onPress={() => Alert.alert('site ruim')}>
-    <Text style={styles.name}>Login</Text> 
-    </TouchableOpacity>
-    <TouchableOpacity
-    style={styles.btnLC}
-    onPress={() => Alert.alert('ste mto ruim')}>
-    <Text style={styles.name}>Cadastro</Text> 
-     </TouchableOpacity>
-    </View>
-    </View>
-  )};
-export default App
+    </NavigationContainer>
+  )
+}
