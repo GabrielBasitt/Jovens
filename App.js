cdf159db378a12cd76409b99d7fd120b87a6e45f
Abílio Gama <94566332+AbilioGamaNJ@users.noreply.github.com>
2022-06-10 17:29:06 -0300

    app novin

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/stack';
+
+import PaginaInicial from './src/components/PaginaInicial'
+import Login from './src/components/Login'
+import CadastroUsuarioScreen from './src/components/CadastroUsuario'
+import Home from './src/components/Routes.js/Home'
+import Cadastros from './src/components/Routes.js/Cadastros'
+
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='PaginaInicial'>
+        <Stack.Screen name = "PaginaInicial" component = {PaginaInicial} />    
+        <Stack.Screen name = "Login" component = {Login} /> 
+        <Stack.Screen name = "Cadastro" component = {CadastroUsuarioScreen} />
+        <Stack.Screen name = "Home" component = {Home} />
+        <Stack.Screen name = "Cadastros" component = {Cadastros} />
+        
+      </Stack.Navigator>
+      
+    </NavigationContainer>
+  )
+
+  }
