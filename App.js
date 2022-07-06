751bf23f95d92613766a186e7e65e53ff3c7b2e2
dai222j <rstaffk@gmail.com>
2022-07-06 16:43:45 -0300

    oi

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import Login from './src/components/Login'
 import CadastroUsuarioScreen from './src/components/CadastroUsuario'
 import Home from './src/components/Routes.js/Home'
 import Cadastros from './src/components/Routes.js/Cadastros'
+import Feed from './src/components/Feed';
+
 
 
 const Stack = createStackNavigator();
@@ -14,8 +16,8 @@ export default function App() {
   
   return(
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='PaginaInicial'>
-        <Stack.Screen name = "PaginaInicial" component = {PaginaInicial}  options={{headerShown:false}}/>    
+      <Stack.Navigator initialRouteName='Feed'>
+      <Stack.Screen name = "Feed" component = {Feed}  options={{headerShown:false}}/>
         <Stack.Screen name = "Login" component = {Login} /> 
         <Stack.Screen name = "Cadastro" component = {CadastroUsuarioScreen} />
         <Stack.Screen name = "Home" component = {Home} />
@@ -24,6 +26,9 @@ export default function App() {
       </Stack.Navigator>
       
     </NavigationContainer>
+    
+   
   )
 
   }
+//<Stack.Screen name = "PaginaInicial" component = {PaginaInicial}  options={{headerShown:false}}/>
