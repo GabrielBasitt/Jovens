d47d5e8c9ea19ef2b6f4ece004c0e30c3ea648bf
GabrielBasitt <gabriel_basitt@estudante.sesisenai.org.br>
2022-07-09 16:50:58 -0300

    morrer

--- src/components/CadastroUsuario.js
+++ src/components/CadastroUsuario.js
@@ -6,9 +6,7 @@ import React from "react";
 
 const bolaVerde = require('../../assets/BolaVerdeEsquerda.png')
 
-export default function CadastroUsuarioScreen({ navigation }) {
-  
-
+const cadastroUsuario = (props) => {
     const [nome_completo, setNome_completo] = useState('')
     const [cpf, setCpf] = useState('')
     const [telefone, setTelefone] = useState('')
@@ -16,14 +14,21 @@ export default function CadastroUsuarioScreen({ navigation }) {
     const [email, setEmail] = useState('')
     const [senha, SetSenha] = useState('')
 
+    const handleNome_CompletoChange = nome_completo => setNome_completo(nome_completo)
+    const handleCpfChange = cpf => setCpf(cpf)
+    const handleTelefoneChange = telefone => setTelefone(telefone)
+    const handledata_nascimentoChange = data_nascimento => setData_nascimento(data_nascimento)
+    const handleEmailChange = email => setEmail(email)
+    const handleSenhaChange = senha => SetSenha(senha)
+
     const postUser = async () => {
         
-    if (nome_completo  && cpf && telefone && data_nascimento && email != "" && senha != "") {
+    if (nome_completo && cpf && telefone && data_nascimento && email && senha != "") {
         try {
-            const requestOptons = {
-                method: 'post',
-                Headers: { 'Content-type': 'application/json' },
-                body:JSON.stringify({
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-type': 'application/json' },
+                body: JSON.stringify({
                     nome_completo: nome_completo,
                     cpf: cpf,
                     telefone: telefone,
@@ -32,9 +37,9 @@ export default function CadastroUsuarioScreen({ navigation }) {
                     senha: senha
                 })
             }
-            await fetch('https://jovens-server.herokuapp.com/pessoa', requestOptons)
-            navigation.navigate('Login')
-
+            await fetch('https://jovens-db.herokuapp.com/pessoa', requestOptions)
+            props.addUser()
+            //navigation.navigate('Login')
            
         }catch( error){
             console.log(error)
@@ -64,40 +69,56 @@ export default function CadastroUsuarioScreen({ navigation }) {
                     <View style={styles.viewInputs}>
 
                         <TextInput style={styles.inpName}
-                            placeholder="Insira seu nome completo..."
-                            autoCorrect={false}
-                            onChange={(nome_completo) => { setNome_completo(nome_completo.target.value)}
-                    
-                           }/>
-
+                        //    value={nome_completo}
+                           placeholde={'Escreva um nome'}
+                            //  onChaanngeText={hdleNome_CompletoChange}
+                        onChangeText={(nome_completo) => { setNome_completo(nome_completo.target.value)} }
+                           />
                         <TextInput style={styles.inpCPF}
-                            placeholder="Insira seu CPF..."
-                            autoCorrect={false}
-                            onChange={(cpf) => { setCpf(cpf.target.value)} 
-                            }/>
+                            // placeholder="Insira seu CPF..."
+                            // autoCorrect={false}
+                            // onChange={(cpf) => {setCpf(cpf.target.value)} 
+                            value={nome_completo}
+                            placeholde={'Escreva um nome'}
+                              onChangeText={handleNome_CompletoChange}
+                            //}
+                            />
 
                         <TextInput style={styles.inpData}
-                            placeholder="Insira sua data de nascimento..."
-                            autoCorrect={false}
-                            onChange={(data_nascimento) => { setData_nascimento(data_nascimento.target.value)} 
-                            }/>
+                            // placeholder="Insira sua data de nascimento..."
+                            // autoCorrect={false}
+                            // onChange={(data_nascimento) => { setData_nascimento(data_nascimento.target.value)} 
+                            value={nome_completo}
+                            placeholde={'Escreva um nome'}
+                              onChangeText={handleNome_CompletoChange}
+                            //}
+                            />
 
                         <TextInput style={styles.inpTelefone}
-                            placeholder="Insira seu telefone..."
-                            autoCorrect={false}
-                            onChange={(telefone) => { setTelefone(telefone.target.value)} 
-                            }/>
+                            // placeholder="Insira seu telefone..."
+                            // autoCorrect={false}
+                            // onChange={(telefone) => { setTelefone(telefone.target.value)} 
+                            value={nome_completo}
+                            placeholde={'Escreva um nome'}
+                              onChangeText={handleNome_CompletoChange}
+                            //}
+                            />
 
                         <TextInput style={styles.inpEmail}
-                            placeholder="Insira seu E-mail..."
-                            autoCorrect={false}
-                            onChange={(email) => { setEmail(email.target.value)} 
-                            }/>
+                            // placeholder="Insira seu E-mail..."
+                            // autoCorrect={false}
+                            // onChange={(email) => { setEmail(email.target.value)} 
+                           // }
+                           value={nome_completo}
+                           placeholde={'Escreva um nome'}
+                             onChangeText={handleNome_CompletoChange}
+                            />
 
                         <TextInput style={styles.inpSenha}
-                            placeholder="Insira uma senha..."
-                            autoCorrect={false}
-                            onChange={ (senha) => { SetSenha(senha.target.value)}} 
+                            // placeholder="Insira uma senha..."
+                            // autoCorrect={false}
+                            // onChange={ (senha) => { SetSenha(senha.target.value)}}
+
                             />
 
                         <TouchableOpacity
@@ -114,4 +135,4 @@ export default function CadastroUsuarioScreen({ navigation }) {
     )
 
     }
-
+export default cadastroUsuario 
