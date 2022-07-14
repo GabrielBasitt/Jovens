3584751e6055a3dd61ee1b99c5966f00d5f6e9db
AbilioGamaNJ <abiliogamanetojunior@gmail.com>
2022-07-14 16:54:28 -0300

    mudanças novinhas

--- src/components/Routes.js/Home.js
+++ src/components/Routes.js/Home.js
@@ -7,7 +7,9 @@ import Post from '../Post'
 import {MaterialCommunityIcons, Feather} from '@expo/vector-icons';
 import { createStackNavigator } from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/Ionicons';
-
+import {useTheme} from 'react-native-paper';
+import EditarPerfilScreen from "../EditarPerfilScreen";
+import {View} from "react-native"
 
 const ProfileStack = createStackNavigator();
 const Tab = createBottomTabNavigator();
